71070a25681c3a9f7fb2aac979e85434650df8a7
Agenesis13 <santos13akuma@gmail.com>
2024-11-11 13:01:44 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'port' => 3306,
+    'database' => 'acp3e3jf0ehsxcbl',
+    'username' => 'as8boonishjaaqbr',
+    'password' => 'v3u0s0peliepvlus',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://localhost/flarum/public',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
